e083f7cfb20d3efdaf85d688a0c12ff7f71421a3
igate solutions <igatecomputereducation@gmail.com>
2023-06-13 08:23:43 +0530

    v1

---
+++ prog167.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int a[50],n,i,pos;
+	
+	printf("How many elements:");
+	scanf("%i",&n);
+	
+	printf("Enter elements\n");
+	for(i=0;i<n;i++)
+		scanf("%i",&a[i]);
+		
+	printf("Position to delete:");
+	scanf("%i",&pos);
+	
+	if(pos<1 || pos>n)
+		printf("Invalid position");
+	else
+	{
+		pos--;
+		
+		for(i=pos;i<n-1;i++)
+			a[i]=a[i+1];
+		
+		n--;
+		
+		printf("Result array..\n");
+		for(i=0;i<n;i++)
+			printf("%i\t",a[i]);
+	}
+	return 0;
+}
---
+++ prog168.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int a[50],n,i,k,temp;
+	
+	printf("How many elements:");
+	scanf("%i",&n);
+	
+	printf("Enter elements\n");
+	for(i=0;i<n;i++)
+		scanf("%i",&a[i]);
+		
+	for(k=0;k<n-1;k++)
+	{
+		for(i=k+1;i<n;i++)
+		{
+			if(a[i]<a[k])
+			{
+				temp=a[i];
+				a[i]=a[k];
+				a[k]=temp;
+			}
+		}
+	}
+
+	printf("Elements in ascending order\n");
+	for(i=0;i<n;i++)
+		printf("%i\t",a[i]);
+	return 0;
+}
